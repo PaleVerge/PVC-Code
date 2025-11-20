6dac33042165809e1588b784fdd1096cb5f70cfc
PaleVerge <fnme370679@gmail.com>
2025-11-21 00:57:16 +0800

    字符串数根

---
+++ 高斯数列.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void){
+
+    int n,sum;
+    while (scanf("%d",&n)!=EOF) {
+        sum=0;
+        for(int i=1;i<=n;i++){
+            sum+=i;
+        }printf("%d\n\n",sum);
+    }
+    return 0;
+}
