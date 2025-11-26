01e330c5c19eee8397be3eccf7014442fc1bca01
PaleVerge <fnme370679@gmail.com>
2025-11-26 17:06:07 +0800

    小写字母替换

---
+++ 小写字母替换.c
@@ -0,0 +1,23 @@
+//
+// Created by rint on 2025/11/26.
+//
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+int main(void) {
+    char str[101];
+    int len,i;
+    while (gets(str)!=NULL) {
+        len=strlen(str);
+        for (i=0;i<len;i++) {
+            if (isalpha(str[i]) && str[i]>80) {
+                printf("*");
+            }else {
+                printf("%c",str[i]);
+            }
+        }
+        putchar('\n');
+    }
+
+    return 0;
+}
