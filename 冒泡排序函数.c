7b51031ba99bd99b862d467eda4a452d05b6ce8c
PaleVerge <fnme370679@gmail.com>
2025-12-13 01:06:36 +0800

    冒泡排序函数

--- 冒泡排序函数.c
+++ 冒泡排序函数.c
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #define N 5
 void bubbleSort(int nums[N]) {
-    int i,j,tmp;
     for (int i = N - 1; i > 0; i--) {
         for (int j = 0; j < i; j++) {
             if (nums[j] > nums[j + 1]) {
@@ -17,7 +16,6 @@ void bubbleSort(int nums[N]) {
 }
 int main(void) {
     int a[5];
-    sort
     for (int i = 0; i < 5; i++) {
         scanf("%d",&a[i]);
     }
